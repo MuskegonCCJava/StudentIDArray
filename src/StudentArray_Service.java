6afe3ff6514485cf261567ccf1da50c975528a77
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-03-18 00:25:57 -0400

    deleted an element

--- src/StudentArray_Service.java
+++ src/StudentArray_Service.java
@@ -1,7 +1,7 @@
 
 public class StudentArray_Service {
 	
-	private int[] studentID = {1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010};
+	private int[] studentID = {1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009};
 	private String[] fname = {"Kyle", "Alex", "Chris", "Bob", "John", "Julie", "Kaitlyn", "Macinda", "David", "Sam"};
 	private float[] gpa = {3.5f, 3.0f, 2.5f, 2.3f, 3.6f, 4.0f, 3.7f, 2.4f, 2.9f, 3.9f };
 	
